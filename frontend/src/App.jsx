74a04fa0b1dc1c06c9cdb70c729101fa334f0180
kush <kushg231@gmail.com>
2025-07-20 11:43:54 +0000

    m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Homepage from './pages/Homepage'
 const App = () => {
   return (
-    <div></div>
+    <Homepage />
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,20 @@ import { Link } from "react-router"
 
 const Navbar = () => {
   return (
-    <header className="">
-      Subscription App
-    </header>
-  )
+    <div className="navbar bg-base-300 shadow-md px-4">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">SubSync</a>
+
+          </div>
+          <div className="flex-none gap-2"><ul className="menu menu-horizontal px-1 hidden md:flex">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/subscriptions">My Subscriptions</Link></li>
+          <li><Link to="/dashboard">Company Dashboard</Link></li>
+        </ul></div>
+
+
+    </div>
+  );
 }
 
 export default Navbar
